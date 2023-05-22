3c7d94ec90daac1c92f7cefa00c3b2788502533c
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-22 13:30:32 -0300

    new

---
+++ src/database/sqlite/migration/index.js
@@ -0,0 +1,13 @@
+const APIPlugin = require('webpack/lib/APIPlugin');
+const sqliteConnection = require('../../sqlite')
+const createUsers = require('./createUsers')
+
+async function migrationRun(){
+  const schemas = [
+    createUsers
+  ].join('');
+
+  sqliteConnection().then(db => db.exec(schemas)).catch(error => console.error(error));
+}
+
+module.exports = migrationRun;
---
+++ src/server.js
@@ -0,0 +1,43 @@
+require("express-async-errors");
+require("dotenv/config")
+const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
+
+const cors = require("cors")
+const migrationRun = require("./database/sqlite/migration")
+const express = require("express");
+
+const routes = require("./routes/index.js")
+
+migrationRun();
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error);
+  
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+
+});
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
